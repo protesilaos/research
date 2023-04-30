7b8ce15bfd9b74c5bdb0a75525aac71dda34c636
Protesilaos Stavrou <info@protesilaos.com>
2023-04-30 20:39:52 +0300

    Experiment with process :connection-type as a pipe
    The docs suggest that pipes can be more efficient, especially when
    the user is not meant to interact with the process while it is
    running, such as in our presumes cases.
    
    Evaluate: (info "(elisp) Asynchronous Processes")

--- research.el
+++ research.el
@@ -129,6 +129,7 @@ subsequently renamed to include BUFFER-NAME and a timestamp."
     ;; FIXME 2023-04-23: Make it asynchronous.
     (make-process
      :name "research"
+     :connection-type 'pipe
      :buffer stdout-buffer
      :command (research--prepare-shell-invocation arguments)
      ;; FIXME 2023-04-23: Make the sentinel its own function.
