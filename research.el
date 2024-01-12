f74cd518b27107f8d05541a3adbad5d3600ef0a8
Protesilaos Stavrou <info@protesilaos.com>
2024-01-12 08:32:34 +0200

    Update copyright years

--- research.el
+++ research.el
@@ -1,6 +1,6 @@
 ;;; research.el --- Run shell searches and store them reproducibly in buffers -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023  Free Software Foundation, Inc.
+;; Copyright (C) 2023-2024  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou General Issues <~protesilaos/general-issues@lists.sr.ht>
